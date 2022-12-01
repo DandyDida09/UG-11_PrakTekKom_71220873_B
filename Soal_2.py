8f24d8e15b429d062322d8abbe51518a42033994
DandyDida09 <117884467+DandyDida09@users.noreply.github.com>
2022-12-01 12:49:11 +0700

    Add files via upload

---
+++ Soal_2.py
@@ -0,0 +1,14 @@
+x = input('masukan kata: katak')
+palindrom = True
+
+panjang_x = len(x)
+
+for i in range (panjang_x//2):
+    if (x[i] != x[panjang_x-i-1]):
+        palindrom = False
+        break
+
+if palindrom:
+    Print(x,'adalah palindrome!')
+else:
+    print(x,'bukan palindrome!'); 
